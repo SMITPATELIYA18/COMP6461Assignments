1a7b9cbdf90bafce88d118f769b59453c3fc6c4a
Smit Pateliya <smitpa2000@gmail.com>
2022-09-22 19:17:49 -0400

    Help Command Added

---
+++ Assignment1/src/main/java/Httpc.java
@@ -0,0 +1,5 @@
+public class Httpc {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ Assignment1/src/main/java/HttpcLibrary.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+public class HttpcLibrary {
+
+    private final String HTTPC = "httpc";
+
+    public HttpcLibrary() {}
+
+    public void help(List<String> parameters) {
+        if(!parameters.get(0).equals(HTTPC)) {
+            System.out.println("Please, Enter correct command!!!");
+            return;
+        }
+        if(!parameters.get(1).equals("help")) {
+            System.out.println("Please, Check your command!!!");
+            return;
+        }
+        if(parameters.size() == 2) {
+            System.out.println("httpc is a curl-like application but supports HTTP protocol " +
+                    "only.\n" +
+                    "Usage: \n\thttpc command [arguments] \nThe commands are: \n\tget " +
+                    "executes a " +
+                    "HTTP GET request and prints the response.\n\t post executes a HTTP POST" +
+                    "request and prints the response. \n\thelp prints this screen. \n\nUse " +
+                    "\"httpc " +
+                    "help [command]\" for more information about a command.");
+            return;
+        }
+        if(parameters.get(2).equals("get")) {
+            System.out.println("usage: httpc get [-v] [-h key:value] URL \nGet executes a " +
+                    "HTTP GET request for a given URL. \n\t-v Prints the detail of the " +
+                    "response such as protocol, status, and headers. \n\t-h key:value " +
+                    "Associates headers to HTTP Request with the format 'key:value'.");
+            return;
+        }
+        System.out.println("usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file]" +
+                " URL \nPost executes a HTTP POST request for a given URL with inline data " +
+                "or from file. \n\t-v Prints the detail of the response such as protocol, " +
+                "status, and headers. \n\t-h key:value Associates headers to HTTP Request " +
+                "with the format 'key:value'. \n\t-d string Associates an inline data to the" +
+                " body HTTP POST request. \n\t-f file Associates the content of a file to " +
+                "the body HTTP POST request. \n\tEither [-d] or [-f] can be used but not " +
+                "both.");
+    }
+}
